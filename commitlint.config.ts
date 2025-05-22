d993c0c5ff1ba89c5db98fc759a94f8b3632cc86
Joe Takara <44985154+joetakara@users.noreply.github.com>
2025-05-22 07:29:05 +0700

    chore(repo): initial commit (#4)

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,7 +4,11 @@ const commitlintConfig: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   helpUrl: "https://www.conventionalcommits.org/en/v1.0.0",
   rules: {
+    "type-empty": [2, "never"],
     "type-case": [2, "always", "lower-case"],
+    "scope-empty": [2, "never"],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
     "subject-case": [2, "always", "lower-case"],
     "type-enum": [
       2,
